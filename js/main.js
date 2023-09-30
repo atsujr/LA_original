41a033a243456e4a8af0dc30036e183f354ab748
Atsuhiro Muroyama <atuhiro.duda@gmail.com>
2023-09-30 16:10:16 +0900

    add: make second view

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function ($) {
+    console.log("awrga")
+    $('.building-list-images').slick({
+        // slideというクラスに、slick関数を実行するイメージ
+        adaptiveHeight: false,
+        autoplay: false,
+        dots: false,
+        infinite: true,
+        centerMode: true,
+        centerPadding: "20%"
+    });
+});
